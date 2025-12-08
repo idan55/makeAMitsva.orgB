ebd2d8f3299121abf8f6f25521da6f9cd158b41d
idan55 <hadadidan7@gmail.com>
2025-12-08 12:36:34 +0200

    changed index a bbit

--- index.js
+++ index.js
@@ -77,11 +77,6 @@ mongoose
     process.exit(1);
   });
 
-if (uri) {
-  app.listen(PORT, () => {
-    console.log("Server is running on port: ", PORT);
-  });
-} else {
-  console.error("No uri found in env file (MONGODB_URI is missing)");
-  process.exit(1);
-}
+app.listen(PORT, () => {
+  console.log("Server is running on port: ", PORT);
+});
